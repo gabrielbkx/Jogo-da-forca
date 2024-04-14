05e253abcea36f114e59a9557698c11e8b7f5f76
Gabriel de oliveira <141791685+gabrielbkx@users.noreply.github.com>
2024-04-14 16:10:11 -0300

    Inserindo código

---
+++ Jogo da forca.py
@@ -0,0 +1,93 @@
+import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+palavras = ["abóbora", "girassol","piano",]
+
+palavra_aleatoria = random.choice(palavras)
+print(palavra_aleatoria)
+
+display = []
+
+vidas = 6
+
+for letras in palavra_aleatoria:
+    display += "_"
+
+fim_de_jogo = False
+
+while not fim_de_jogo:
+
+    letra = input("Adivinhe uma letra\n").lower()
+
+    for posicao in range (len(palavra_aleatoria)):
+       letras = palavra_aleatoria[posicao]
+
+       if letras == letra:
+            display[posicao] = letras
+            print(display)
+
+    if "_" not in display:
+        fim_de_jogo = True
+        print("Você Ganhou.")
+
+    if letra not in palavra_aleatoria:
+        vidas -= 1
+        print(stages[vidas])
+
+        if vidas == 0:
+            fim_de_jogo = True
+            print("Você Perdeu.")
